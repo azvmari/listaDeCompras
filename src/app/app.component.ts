4feddd398299e02022ae8ed36b0138528103661d
azvmari <202111263@uesb.edu.br>
2024-10-08 12:46:33 -0300

    Criação e exibição da lista de itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "./components/header/header.component";
 import { TextInputComponent } from "./components/text-input/text-input.component";
 import { ItemComponent } from "./components/item/item.component";
@@ -7,12 +8,18 @@ import { ItemComponent } from "./components/item/item.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TextInputComponent, ItemComponent],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    TextInputComponent, 
+    ItemComponent,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 
 export class AppComponent {
-  title = 'listaDeCompras';
+  list = ["Item 1", "Item 2"]
 }
--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
@@ -9,5 +9,5 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './item.component.css'
 })
 export class ItemComponent {
-
+  @Input() name!: string
 }
