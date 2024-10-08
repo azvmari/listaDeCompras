7f67810696c42f0dc90f3a62c2f801eaafeb2cd9
azvmari <202111263@uesb.edu.br>
2024-10-08 13:19:09 -0300

    remove component button

--- src/app/components/text-input/text-input.component.ts
+++ src/app/components/text-input/text-input.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { ButtonComponent } from "../button/button.component";
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-text-input',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ReactiveFormsModule, MatIconModule],
   templateUrl: './text-input.component.html',
   styleUrl: './text-input.component.css'
 })
 export class TextInputComponent {
-
+  @Input() handleAdd !: (name: string) => void
+  formControl = new FormControl('');  
 }
