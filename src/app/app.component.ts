55b047ca922da93add9b6bf9029fac9839f61d7a
azvmari <202111263@uesb.edu.br>
2024-10-08 01:06:51 -0300

    criação da lista de itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { TextInputComponent } from "./components/text-input/text-input.component";
+import { ItemComponent } from "./components/item/item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TextInputComponent],
+  imports: [RouterOutlet, HeaderComponent, TextInputComponent, ItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
