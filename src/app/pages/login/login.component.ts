4b50d3fd02d41a86c010c0c3f9c53cdaebbd5a3f
azvmari <202111263@uesb.edu.br>
2024-11-05 20:47:54 -0300

    refatoração da tela de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
@@ -16,13 +15,11 @@ import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 export class LoginComponent {
   faGoogle = faGoogle
 
-  formControl = new FormGroup({
-    name: new FormControl('', Validators.required)
-  })
+  loginWithGoogle() {
+    console.log("google")
+  }
 
-  onSubmit() {
-    if (this.formControl.valid && this.formControl.value.name) {
-      this.formControl.reset();
-    }
+  loginWithFacebook() {
+    console.log("facebook")
   }
 }
