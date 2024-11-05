364e6997eecec02f3ef54f2f7e7c67d1b3adebbe
azvmari <202111263@uesb.edu.br>
2024-11-05 20:41:52 -0300

    estilização da tela de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, MatIconModule, FontAwesomeModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  faGoogle = faGoogle
+
+  formControl = new FormGroup({
+    name: new FormControl('', Validators.required)
+  })
+
+  onSubmit() {
+    if (this.formControl.valid && this.formControl.value.name) {
+      this.formControl.reset();
+    }
+  }
 }
